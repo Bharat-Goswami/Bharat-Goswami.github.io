7db210884b73f76a9210d0cdbbb8ba08bbd068dc
Bharat-Goswami <bharatgoswami2112@gmail.com>
2023-12-21 13:55:02 +0530

    reset

--- index.js
+++ index.js
@@ -1,21 +1,6 @@
 let hamMenuIcon = document.getElementById("ham-menu");
 let navBar = document.getElementById("nav-bar");
 let navLinks = navBar.querySelectorAll("li");
-const text = document.querySelector("#title");
-
-
-
-
-
-const load = ()=>{
-  setTimeout(()=>{
-    text.textContent = "I am a Full Stack Web Developer. "
-  },0);
-}
-
-load();
-setInterval(() => {
-}, 12000);
 
 hamMenuIcon.addEventListener("click", () => {
   navBar.classList.toggle("active");
@@ -31,7 +16,7 @@ navLinks.forEach((navLinks) => {
 
 function openResume() {
   window.open(
-    "https://drive.google.com/file/d/1qnen_Ze6CoP6YR9Ya3IJ2SLaeklLSj2B/view?usp=sharing",
+    'https://drive.google.com/file/d/1qnen_Ze6CoP6YR9Ya3IJ2SLaeklLSj2B/view?usp=sharing',
     '_blank'
   );
 }
@@ -64,72 +49,6 @@ function saveFile(e) {
   Formdata.push(data)
   localStorage.setItem("portfolioData", JSON.stringify(Formdata))
 }
-
-function toggleTheme() {
-  const themeStyleLink = document.getElementById('theme-style');
-  
-  if (themeStyleLink.getAttribute('href') === 'index.css') {
-      themeStyleLink.setAttribute('href', 'index-dark.css');
-  } else {
-      themeStyleLink.setAttribute('href', 'index.css');
-  }
-}
-
-
-
-// document.addEventListener("DOMContentLoaded", function() {
-//   const form = document.querySelector("#contact-subdiv2 form");
-
-//   form.addEventListener("submit", function(event) {
-//       event.preventDefault(); // Prevent default form submission
-
-//       // Fetch form data
-//       const formData = new FormData(form);
-
-//       // Example: Send form data to a backend endpoint using Fetch API
-//       fetch("https://your-backend-url.com/sendEmail", {
-//           method: "POST",
-//           body: formData
-//       })
-//       .then(response => {
-//           // Handle response
-//           if (response.ok) {
-//               // Show success message or redirect the user
-//               console.log("Message sent successfully!");
-//               // Example: Redirect to a thank you page
-//               window.location.href = "thankyou.html";
-//           } else {
-//               // Handle error
-//               console.error("Failed to send message.");
-//           }
-//       })
-//       .catch(error => {
-//           console.error("Error:", error);
-//       });
-//   });
-// });
-
-
-// Get all the section elements
-// const sections = document.querySelectorAll('section');
-// const sections = document.querySelectorAll("#home, #about, #skills, #projects, #github, #contact, #footer");
-
-
-// // Get all the navigation links
-// const navScroll = document.querySelectorAll('#nav-bar a');
-
-// // Function to highlight the current section in the navigation menu
-// function highlightNavLink() {
-//     let index = sections.length;
-
-//     while (--index && window.scrollY + 50 < sections[index].offsetTop) {}
-
-//     navScroll.forEach(link => link.classList.remove('active'));
-//     navScroll[index].classList.add('active');
-// }
-
-// // Add event listener for scroll
-// window.addEventListener('scroll', highlightNavLink);
 
 const sections = document.querySelectorAll("#home, #about, #skills, #projects, #github, #contact, #footer");
 
