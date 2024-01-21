493609125526fadd1896dffe3a4d8b6aece8b984
Bharat-Goswami <bharatgoswami2112@gmail.com>
2024-01-21 14:36:15 +0530

    send email to user is added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 let hamMenuIcon = document.getElementById("ham-menu");
 let navBar = document.getElementById("nav-bar");
 let navLinks = navBar.querySelectorAll("li");
@@ -69,4 +70,77 @@ function highlightNavLink() {
 // Add event listener for scroll
 window.addEventListener('scroll', highlightNavLink);
 
+// Add the feedback to the database
 
+document.getElementById('contactForm').addEventListener('submit', function (event) {
+  event.preventDefault(); // Prevent the default form submission
+  
+  // Get form values
+  const name = document.getElementById('fname').value;
+  const email = document.getElementById('email').value;
+  const message = document.getElementById('subject').value;
+
+  // Create an object with the form data
+  const formData = {
+    name: name,
+    email: email,
+    message: message
+  };
+
+  // Make a POST request to your server
+  fetch('https://potpholio.onrender.com/users/add', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(formData),
+  })
+  .then(response => response.json())
+  .then(data => {
+    if(data.error){
+      showtost("Error");
+    }
+    else{
+      showtost("Succesful");
+    }
+    sendEmail(formData);
+    console.log('Success:', data);
+
+    // Clear the form fields
+    document.getElementById('fname').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('subject').value = '';
+
+    // Optionally, you can also focus on the first field
+    document.getElementById('fname').focus();
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+    // Optionally, handle the error here
+  });
+});
+
+// tost box
+var tostBox = document.getElementById("tostBox");
+function showtost(cre){
+  console.log(cre)
+  var tost = document.createElement("div");
+  tost.classList.add("tost");
+  tost.innerHTML = "<i class='bx bxs-x-circle'></i>" +"<h2>Oops Try with different name or Email id</h2>";
+  if(cre.includes('Succesful')){
+    tost.classList.add("sucess");
+    tost.innerHTML = "<i class='bx bx-check-circle'></i>" +"<h2>That you your feedback is appriciated and please check your email😊</h2>";
+  }
+  tostBox.appendChild(tost);
+  setTimeout(()=>{
+    tost.remove();
+  },2000);
+
+}
+
+
+// Email JS
+
+function sendEmail(formData){
+    emailjs.send('service_30bvx1p','template_th7030g',formData)
+}
